620827e261b9ae6a833ebe608c80edca2e4a9d9b
Hugo Morales <hmoralesg@icloud.com>
2017-06-13 19:19:15 -0400

    2 - Ejercicio 1

--- 2 ciclos/ejercicio1.rb
+++ 2 ciclos/ejercicio1.rb
@@ -1,5 +1,3 @@
-# En el siguiente código reemplaza la instrucción 'for' por 'times'
+# En el siguiente codigo reemplaza la instrucción 'for' por 'times'
 
-for i in 1..10 do
-  puts i
-end
+10.times { |i| puts i + 1 }
