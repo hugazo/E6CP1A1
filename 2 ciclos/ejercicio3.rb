a179271dc52d5dbafcb6b81e3afd7eb91cb954b0
Hugo Morales <hmoralesg@icloud.com>
2017-06-13 19:27:26 -0400

    2 - Ejercicio 3

--- 2 ciclos/ejercicio3.rb
+++ 2 ciclos/ejercicio3.rb
@@ -1,2 +1,14 @@
 # Mostrar todos los divisores del número 990 con:
 # while, for, times.
+
+990.times { |i| puts i+1 if (990 % (i + 1)).zero? }
+
+for i in 1..990
+  puts i if (990 % i).zero?
+end
+
+i = 1
+while i <= 990
+  puts i if (990 % i).zero?
+  i += 1
+end
