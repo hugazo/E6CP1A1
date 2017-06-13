9f16ce5f28072ac71d6e8c24c0abbcaeffd32680
Hugo Morales <hmoralesg@icloud.com>
2017-06-13 19:50:39 -0400

    2 - Ejercicio 9

--- 2 ciclos/ejercicio9.rb
+++ 2 ciclos/ejercicio9.rb
@@ -11,3 +11,14 @@
 </table>
 
 =end
+def crearTabla(numeroFilas)
+  content = "<table>\n"
+  content += "\t<tbody>\n"
+  content += "\t\t<trow>\n"
+  numeroFilas.times { |i| content += "\t\t\t<td> #{i+1} </td>\n" }
+  content += "\t\t</trow>\n"
+  content += "\t</tbody>\n"
+  content += "</table>\n"
+end
+
+puts crearTabla(4)
