dbf314288debc6face48e86dc984b5fc7ff707dd
Hugo Morales <hmoralesg@icloud.com>
2017-06-13 20:31:24 -0400

    3 - Ejercicio 3

--- 3 ciclos anidados/ejercicio3.rb
+++ 3 ciclos anidados/ejercicio3.rb
@@ -2,3 +2,14 @@
 # la tabla de multiplicar del número ingresado. Debe repetir la operación hasta
 # que se ingrese un 0 (cero).
 # Ingrese un número (0 para salir): _
+
+opcion = 1
+while opcion != 0
+  puts "Ingrese numero a calcular su tabla de multiplicar (0 para salir)"
+  opcion = gets.chomp.to_i
+  if opcion != 0
+    10.times { |i| puts "#{opcion} * #{i+1} = #{opcion*(i+1)} " }
+  else
+    puts "Gracias por usar este programa"
+  end
+end
